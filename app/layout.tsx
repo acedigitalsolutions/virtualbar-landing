94b1346617b9f82e54c785ce8c26cc4f6928724f
Francesco Pio Nocerino <nocerpio9@icloud.com>
2026-04-28 18:10:42 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -163,17 +163,6 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${META_PIXEL_ID}');fbq('track','PageView');` }} />
         <noscript><img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`} alt="" /></noscript>
         {/* End Meta Pixel */}
-        <Script
-          id="gtm-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MRLJ68QS');`,
-          }}
-        />
         <script
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(organizationSchema),
@@ -199,16 +188,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           fontSans.variable
         )}
       >
-        <noscript>
-          <iframe
-            height="0"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MRLJ68QS"
-            style={{ display: "none", visibility: "hidden" }}
-            width="0"
-            title="Google Tag Manager"
-          />
-        </noscript>
-        <Providers themeProps={{ attribute: "class", defaultTheme: "system" }}>
+<Providers themeProps={{ attribute: "class", defaultTheme: "system" }}>
           <div className="relative flex flex-col min-h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
